74405cb525d2812dad6070f30b6a36f03a279786
Colby Klein <shakesoda@gmail.com>
2013-01-17 05:11:20 -0800

    Swap shader test and test drawing. Need to make the drawing test resizable later.

--- src/screens/ScreenTestDrawing.cpp
+++ src/screens/ScreenTestDrawing.cpp
@@ -22,69 +22,16 @@ ScreenTestDrawing::ScreenTestDrawing(string name) : Screen(name)
 {
 }
 
-GLuint tex;
-
 void ScreenTestDrawing::Init()
 {
 	glGenVertexArrays(1, &vao);
 	glGenBuffers(2, &buf[0]);
 	
-	shader = new ShaderProgram("Mandelbrot.Vertex.GL30", "Mandelbrot.Fragment.GL30");
+	shader = new ShaderProgram("Generic.Vertex.GL30", "Generic.Fragment.GL30");
 	shader->BindAttrib(0, "vPosition");
 	shader->BindAttrib(1, "vCoords");
 	shader->Link();
 	shader->Bind();
-
-	glGenTextures(1, &tex);
-	glBindTexture(GL_TEXTURE_2D, tex);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-	size_t size = 128;
-	glm::vec4 *pixels = new glm::vec4[size];
-
-	typedef pair<float, glm::vec4> stop;
-
-	vector<stop> stops;
-	stops.push_back(stop(0.00, glm::vec4(0.0, 0.0, 0.0, 1.0)));
-	stops.push_back(stop(1.00, glm::vec4(0.2, 0.5, 1.0, 1.0)));
-
-	size_t prev = 0;
-	size_t next = 0;
-
-	float factor = size / stops.size();
-	for (size_t i = 0; i < size; ++i)
-	{
-		float progress = float(i)/size;
-
-		if (progress > stops[next].first)
-		{
-			prev = next;
-			next++;
-		}
-
-		float prev_scaled = prev * factor;
-		float next_scaled = next * factor;
-
-		float stop_progress = glm::max(float(i), 0.001f);
-		stop_progress -= prev_scaled;
-		stop_progress /= next_scaled;
-
-		glm::vec4 color = glm::mix(
-			stops[prev].second, stops[next].second,
-			stop_progress
-		);
-		pixels[i] = color;
-	}
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, size, 1, 0, GL_RGBA, GL_FLOAT,
-		glm::value_ptr(pixels[0])
-	);
-
-	delete[] pixels;
 
 	glBindVertexArray(vao);
 	
--- tests/shadertest.cpp
+++ tests/shadertest.cpp
@@ -20,18 +20,20 @@ int main(int argc, char **argv)
 	LOG = new Logger("logs/shader-test.txt");
 
 	glfwInit();
+	glfwWindowHint(GLFW_RESIZABLE, 0);
 	GLFWwindow *w = glfwCreateWindow(960, 540, "Shader Test", NULL, NULL);
 	glfwMakeContextCurrent(w);
 	glxwInit();
 
-	ShaderProgram p("Generic.Vertex.GL30", "Generic.Fragment.GL30");
+	ShaderProgram p("Mandelbrot.Vertex.GL30", "Mandelbrot.Fragment.GL30");
 	{
 		p.BindAttrib(0, "vPosition");
-		p.BindAttrib(0, "vCoords");
+		p.BindAttrib(1, "vCoords");
 		p.Link();
 		p.Bind();
 		p.SetMatrix4("ModelViewProjection", glm::mat4(1.0));
 		p.SetVector2("Viewport", glm::vec2(960, 540));
+		p.SetInteger("ColorTable", 0);
 	}
 
 	GLuint vao, buf[2];
@@ -62,6 +64,59 @@ int main(int argc, char **argv)
 		glEnableVertexAttribArray(1);
 	}
 	
+	GLuint tex;
+	{
+		glGenTextures(1, &tex);
+		glBindTexture(GL_TEXTURE_2D, tex);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+		size_t size = 128;
+		glm::vec4 *pixels = new glm::vec4[size];
+
+		typedef pair<float, glm::vec4> stop;
+
+		vector<stop> stops;
+		stops.push_back(stop(0.00, glm::vec4(0.0, 0.0, 0.0, 1.0)));
+		stops.push_back(stop(1.00, glm::vec4(0.2, 0.5, 1.0, 1.0)));
+
+		size_t prev = 0;
+		size_t next = 0;
+
+		float factor = size / stops.size();
+		for (size_t i = 0; i < size; ++i)
+		{
+			float progress = float(i)/size;
+
+			if (progress > stops[next].first)
+			{
+				prev = next;
+				next++;
+			}
+
+			float prev_scaled = prev * factor;
+			float next_scaled = next * factor;
+
+			float stop_progress = glm::max(float(i), 0.001f);
+			stop_progress -= prev_scaled;
+			stop_progress /= next_scaled;
+
+			glm::vec4 color = glm::mix(
+				stops[prev].second, stops[next].second,
+				stop_progress
+			);
+			pixels[i] = color;
+		}
+
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, size, 1, 0, GL_RGBA, GL_FLOAT,
+			glm::value_ptr(pixels[0])
+		);
+
+		delete[] pixels;
+	}
 	CheckError();
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -76,6 +131,8 @@ int main(int argc, char **argv)
 	// cleanup:
 	glDeleteVertexArrays(1, &vao);
 	glDeleteBuffers(2, buf);
+
+	glDeleteTextures(1, &tex);
 
 	p.Cleanup();
 
