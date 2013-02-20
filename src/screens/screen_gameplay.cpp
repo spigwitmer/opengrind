a7f6b107be5701e5beadb9f60e8202195d1553a7
Colby Klein <shakesoda@gmail.com>
2013-02-19 21:34:31 -0800

    Simple free look camera. The rotations are slightly botched. Tested using a PS3 controller on Linux.

--- src/screens/screen_gameplay.cpp
+++ src/screens/screen_gameplay.cpp
@@ -7,6 +7,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/rotate_vector.hpp>
 
 #include "renderer/gl30/shader.h"
 #include "actors/model.h"
@@ -15,8 +16,8 @@ using namespace std;
 
 namespace
 {
-	glm::mat4 rotation(1.0);
-	glm::mat4 roll(1.0);
+	glm::vec3 orientation(0.0, 1.0, 0.0), position(0.0, 10.0, 0.0);
+	glm::vec3 forward(0, 1, 0);
 
 	Nepgear::Model *mdl;
 	ShaderProgram *phong;
@@ -41,6 +42,9 @@ void ScreenGameplay::Init()
 	phong->BindAttrib(2, "vNormal");
 	phong->BindAttrib(3, "vTangent");
 	phong->Link();
+
+	phong->Bind();
+	phong->SetMatrix4("ModelView", glm::mat4(1.0));
 
 	glEnable(GL_DEPTH_TEST);
 }
@@ -69,34 +73,70 @@ bool ScreenGameplay::HandleEvent(const string &name, const IEvent &evt)
 	return true;
 }
 
+struct JoystickDevice {
+	std::string name;
+	int id;
+};
+
 void ScreenGameplay::UpdateInternal(double delta)
 {
-	int num_axes = glfwGetJoystickParam(0, GLFW_AXES);
-	float *axes = new float[num_axes];
-	glfwGetJoystickAxes(0, axes, num_axes);
+	std::vector<JoystickDevice> joysticks;
 
-	glm::vec2 j1 = glm::vec2(-axes[0], -axes[1]);
-	glm::vec2 j2 = glm::vec2(-axes[2], -axes[3]);
-	//LOG->Debug("%f %f", j1.x, j1.y);
+	for (int i = 0; i < GLFW_JOYSTICK_LAST; i++)
+	{
+		if (!glfwGetJoystickParam(i, GLFW_PRESENT))
+			continue;
 
-	rotation = glm::rotate<float>(rotation, j1.x * delta * 50, glm::vec3(0, 0, 1));
-	rotation = glm::rotate<float>(rotation, j1.y * delta * 50, glm::vec3(1, 0, 0));
+		JoystickDevice js;
+		js.name = glfwGetJoystickName(i);
+		js.id = i;
 
-	roll = glm::rotate<float>(roll, j2.x * delta * 50, glm::vec3(0, 1, 0));
+		// Blacklist MBP accelerometers.
+		if (js.name == "applesmc")
+			continue;
+
+		joysticks.push_back(js);
+	}
 
 	glm::mat4 xform(1.0);
-	xform = glm::translate(xform, glm::vec3(-0.5, 3, 0.0));
-	xform = glm::translate(xform, glm::vec3(0.0, 0, -1.5));
-	xform *= rotation;
-	xform *= roll;
-
-	glm::mat4 view = glm::lookAt(glm::vec3(0, -10, 2.5), glm::vec3(0, 0, 0), glm::vec3(0.0, 0.0, 1.0));
-
+	glm::mat4 view(1.0);
 	glm::mat4 projection = glm::perspective(90.0f/2, (1280.f/2.f)/800.f, 0.1f, 100.0f);
 
 	phong->Bind();
-	phong->SetMatrix4("ModelView", view * xform);
 	phong->SetMatrix4("Projection", projection);
+
+	if (joysticks.size() == 0)
+	{
+		view = glm::translate(view, position);
+		phong->SetMatrix4("ModelView", view * xform);
+		return;
+	}
+
+	int num_axes = glfwGetJoystickParam(joysticks[0].id, GLFW_AXES);
+	float *axes = new float[num_axes];
+	glfwGetJoystickAxes(joysticks[0].id, axes, num_axes);
+
+	glm::vec2 j1 = glm::vec2(-axes[0], -axes[1]); // left stick
+	glm::vec2 j2 = glm::vec2(-axes[2],  axes[3]); // right stick
+	glm::vec2 tr = glm::vec2(axes[12]*0.5+0.5, -axes[13]*0.5+0.5); // triggers
+
+	// Z is up.
+	glm::vec3 up = glm::vec3(0, 0, 1);
+	forward = glm::normalize(glm::rotateZ<float>(forward, j2.x));
+	forward = glm::normalize(glm::rotateX<float>(forward, j2.y));
+	glm::vec3 right = glm::cross(forward, up);
+
+	// movement
+	float speed = delta * 3.0;
+	position += forward * (j1.y * speed);
+	position += right * (j1.x * speed);
+	position += up * ((tr.x + (-tr.y)) * speed);
+
+	view = glm::lookAt(-forward, glm::vec3(0), up);
+	view = glm::translate(view, position);
+
+	phong->Bind();
+	phong->SetMatrix4("ModelView", view * xform);
 	delete[] axes;
 }
 
